20512d4994af100bb7c2f91d4738c3e2eedcfc2b
Mike Shertz <shertzm2@gmail.com>
2020-09-05 18:01:36 -0400

    u small configuration changes

--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,8 @@ const { Pool, Client } = require('pg');
 
 module.exports.pool = new Pool({
   user: 'images',
-  host: 'localhost',
+  host: '18.222.176.160',
+  //host: 'localhost',
   database: 'image_carousel',
   max: 1000,
   idleTimeoutMillis: 2000,
@@ -11,6 +12,7 @@ module.exports.pool = new Pool({
 
 module.exports.client = new Client({
   user: 'images',
-  host: 'localhost',
+  host: '18.222.176.160',
+  //host: 'localhost',
   database: 'image_carousel',
 });
